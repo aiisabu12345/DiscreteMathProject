18829e4214e1fa73fa0ecac5d2a05347761e87a8
SATU-YANANISSORN <satu20102006@gmail.com>
2026-03-09 20:39:20 +0700

    edge vertex commit

--- Edge.java
+++ Edge.java
@@ -4,53 +4,53 @@ import java.util.Set;
 
 public class Edge implements Comparable<Edge> {
     private String name;
-    private Set<Vertex> allVertex;
-    private int weight;
+    private Set<Vertex> allVertex;          // เก็บ vertex ทั้งหมด ของ object edge 
+    private int weight;                     
 
-    public Edge(String name) {
+    public Edge(String name) {              // constructor
         this.name = name;
         this.allVertex = new HashSet<>();
     }
 
-    public String getName() {
+    public String getName() {               // คืน attribute name
         return name;
     }
 
-    public void addVertex(Vertex v) {
+    public void addVertex(Vertex v) {       // method เพิ่ม Vertex ใน allVertex
         allVertex.add(v);
     }
 
-    public void setWeight(int weight) {
+    public void setWeight(int weight) {     // setter weight
         this.weight = weight;
     }
 
-    public int getValue() {
+    public int getValue() {                 // คืน attribute weight 
         return weight;
     }
 
-    public Set<Vertex> getAllVertex() {
+    public Set<Vertex> getAllVertex() {     // คืน attribue allVertex
         return allVertex;
     }
 
     @Override
-    public int compareTo(Edge other) {
+    public int compareTo(Edge other) {                      // override method ใช้ในการเปรียนเทียบของ object โดยมี weight เป็นเงื่อนไข
         return Integer.compare(this.weight, other.weight);
     }
 
     @Override
-    public String toString() {
+    public String toString() {              // เขียน override method เพื่อให้ print attribute name ออกมา
         return name;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
+    public boolean equals(Object obj) {      // เขียน override method ให้สามารถใช้การตรวจสอบการเท่ากันของ object ด้วย equals ได้
+        if (this == obj)                     // check obj เป็น self ไหม
             return true;
-        if (obj == null)
+        if (obj == null)                     // check obj เป็น null ไหม
             return false;
-        if (getClass() != obj.getClass())
+        if (getClass() != obj.getClass())    // check type class ของ object
             return false;
-        Edge other = (Edge) obj;
-        return name.equals(other.name);
+        Edge other = (Edge) obj;             // downcast
+        return name.equals(other.name);      // check attribute name ตรงกันไหม
     }
 }
